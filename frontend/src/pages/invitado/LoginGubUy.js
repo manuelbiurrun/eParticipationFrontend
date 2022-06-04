858e0702cf22ee292852a2afeafbdcb54d4bb72e
Manuel Iñaki Biurrun Tebot <manuel.biurrun@fing.edu.uy>
2022-06-03 21:45:44 -0300

    levantando heroku... otra avez

--- frontend/src/pages/invitado/LoginGubUy.js
+++ frontend/src/pages/invitado/LoginGubUy.js
@@ -2,7 +2,7 @@ import React from "react";
 import { LoadingLogin } from "../../components/LoadingLogin";
 
 function LoginGubUy() {
-  console.log("llego hasta aca, hurraaaaaaay!!");
+  console.log("llego hasta aca, hurraaaaay!!");
   //obtener el code y el status del response
   // con el code, ir a buscar el token
   //con el token, ir a buscar el correo del usuario
