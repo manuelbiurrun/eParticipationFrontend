f86ab2ec21f37d3189fce77d89ca8941386df9f0
Manuel Iñaki Biurrun Tebot <manuel.biurrun@fing.edu.uy>
2022-06-11 20:43:59 -0300

    tratando de deployear4

--- frontend/src/services/Requests.js
+++ frontend/src/services/Requests.js
@@ -6,7 +6,7 @@ import axios from "axios";
 // Crear base de Axios "instance"
 const instance = axios.create({
   baseURL:
-    "http://localhost:8080/backend-web/eParticipation",
+    "https://eparticipation.web.elasticloud.uy/eParticipation",
   headers: {
     "Content-Type": "application/json",
   },
