a86705b201c00857826ae1becf9d093b9fa94d48
Manuel Iñaki Biurrun Tebot <manuel.biurrun@fing.edu.uy>
2022-06-11 21:09:10 -0300

    tratando de deployearCors

--- frontend/src/services/Requests.js
+++ frontend/src/services/Requests.js
@@ -9,7 +9,9 @@ const instance = axios.create({
     "https://eparticipation.web.elasticloud.uy/eParticipation",
   headers: {
     "Content-Type": "application/json",
-  },
+    'Access-Control-Allow-Origin': '*',
+    'origin':'x-requested-with',
+    'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',  },
 });
 
 /*
